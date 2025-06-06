e4e50b71444521d6fcb221a0643084fe2c9a923f
steven.schwarz <steven.schwarz@twofold.swiss>
2025-06-06 16:43:47 +0200

    CSS WIP

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import { useAppContext } from "../context/AppContext";
 import LanguageToggle from "./LanguageToggle";
 import ThemeToggle from "./ThemeToggle";
-import translations from "../i18n";
 
 const Navbar = () => {
     const { language } = useAppContext();
-    const t = translations[language];
+
+    const labels = {
+        de: { about: "Über mich", skills: "Skills", contact: "Kontakt" },
+        en: { about: "About", skills: "Skills", contact: "Contact" }
+    };
+
+    const t = labels[language];
 
     return (
         <nav className="navbar">
             <div className="nav-left">
+                <a href="#about" className="logo">Steven</a>
+            </div>
+            <div className="nav-right">
                 <a href="#about">{t.about}</a>
                 <a href="#skills">{t.skills}</a>
                 <a href="#contact">{t.contact}</a>
-            </div>
-            <div className="nav-right">
                 <LanguageToggle />
                 <ThemeToggle />
             </div>
